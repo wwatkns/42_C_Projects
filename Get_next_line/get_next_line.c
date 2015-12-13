9d84acbaa010f90031f5b65c08af536942556e3c
William WATKINS <wwatkins@e1r7p6.42.fr>
2015-12-13 12:14:49 +0100

    More progress

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/12 10:12:17 by wwatkins          #+#    #+#             */
-/*   Updated: 2015/12/13 11:31:27 by wwatkins         ###   ########.fr       */
+/*   Updated: 2015/12/13 11:51:10 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h> // TEMPORARY
 
-static int	ft_checkbuf(char *buf) // WORKING
+static int	ft_checkbuf(char *buf) // Ok
 {
 	int i;
 
@@ -27,13 +27,13 @@ static int	ft_checkbuf(char *buf) // WORKING
 	return (-1);
 }
 
-int		get_next_line(int const fd, char **line)
+int			get_next_line(int const fd, char **line)
 {
 	int			i;
 	int			n;
 	int			ret;
 	char		*buf;
-	static char	*tmp;
+	static char	*tmp = NULL;
 
 	if ((buf = ft_strnew(BUFF_SIZE)) == NULL)
 		return (-1);
@@ -53,14 +53,14 @@ int		get_next_line(int const fd, char **line)
 	return (0);
 }
 
-int		main(void)
+int			main(void)
 {
 	int		fd;
 	char	*line;
 
 	if ((fd = open("text.txt", O_RDONLY)) == -1)
 		printf("OPEN ERROR.\n");
-	line = (char*)malloc(sizeof(char) * 1000);
+	line = ft_strnew(1000);
 	get_next_line(fd, &line);
 	printf("%s", line);
 	return (0);
