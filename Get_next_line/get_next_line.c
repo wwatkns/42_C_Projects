21d6e0b41224ce92c121f40d267258e8852b1798
William WATKINS <wwatkins@e1r7p6.42.fr>
2015-12-14 17:47:45 +0100

    get_next_line functionning with buf allocated dynamically

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/12 10:12:17 by wwatkins          #+#    #+#             */
-/*   Updated: 2015/12/14 15:41:06 by wwatkins         ###   ########.fr       */
+/*   Updated: 2015/12/14 17:46:58 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 int			get_next_line(int const fd, char **line)
 {
 	int			ret;
-	char		buf[BUFF_SIZE + 1];
+	char		*buf;
 	static char	*s[255];
 
+	buf = ft_strnew(BUFF_SIZE);
 	s[fd] = (s[fd] == NULL ? ft_strnew(1) : s[fd]);
-	if (!line || fd < 0 || s[fd] == NULL)
+	if (!line || fd < 0 || s[fd] == NULL || buf == NULL)
 		return (-1);
 	while (!ft_strchr(s[fd], '\n') && (ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = 0;
 		s[fd] = ft_strjoin(s[fd], buf);
 	}
-	if (ret == -1)
-		return (-1);
-	if (ft_strchr(s[fd], '\n') && ret != -1)
+	free(buf);
+	if (ft_strchr(s[fd], '\n'))
 	{
 		*line = ft_strsub(s[fd], 0, ft_strchr(s[fd], '\n') - s[fd]);
 		s[fd] = ft_strsub(s[fd], ft_strchr(s[fd], '\n') - s[fd] + 1,
@@ -36,5 +36,7 @@ int			get_next_line(int const fd, char **line)
 	}
 	else
 		*line = ft_strdup(s[fd]);
+	if (ret == -1)
+		return (-1);
 	return (ret != 0 ? 1 : 0);
 }
--- Get_next_line/main.c
+++ Get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 12:42:07 by wwatkins          #+#    #+#             */
-/*   Updated: 2015/12/14 13:35:13 by wwatkins         ###   ########.fr       */
+/*   Updated: 2015/12/14 17:34:59 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ int         main(int argc, char **argv)
 	if ((fd = open(argv[1], O_RDONLY)) == -1)
 		printf("OPEN ERROR.\n");
 	line = ft_strnew(1000);
+//	get_next_line(fd, &line);
+//	printf("%s", line);
 	while (get_next_line(fd, &line) > 0)
 	{
-		printf("");
 		ft_putstr(line);
 		ft_putchar('\n');
 	}
