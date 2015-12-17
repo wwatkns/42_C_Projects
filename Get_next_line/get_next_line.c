d7f3e3f59d27e70ba8a930b57df0b6d382f4f4dd
William WATKINS <wwatkins@e1r7p6.42.fr>
2015-12-17 10:09:18 +0100

    Get_next_line finished with 0 leaks

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/12 10:12:17 by wwatkins          #+#    #+#             */
-/*   Updated: 2015/12/14 17:46:58 by wwatkins         ###   ########.fr       */
+/*   Updated: 2015/12/17 10:03:48 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,27 @@
 int			get_next_line(int const fd, char **line)
 {
 	int			ret;
-	char		*buf;
+	char		*del;
+	char		buf[BUFF_SIZE + 1];
 	static char	*s[255];
 
-	buf = ft_strnew(BUFF_SIZE);
-	s[fd] = (s[fd] == NULL ? ft_strnew(1) : s[fd]);
-	if (!line || fd < 0 || s[fd] == NULL || buf == NULL)
+	if (!line || fd < 0 || !(s[fd] = (s[fd] == NULL ? ft_strnew(1) : s[fd])))
 		return (-1);
 	while (!ft_strchr(s[fd], '\n') && (ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		buf[ret] = 0;
-		s[fd] = ft_strjoin(s[fd], buf);
+		buf[ret] = '\0';
+		s[fd] = ft_strjoin((del = s[fd]), buf);
+		free (del);
 	}
-	free(buf);
-	if (ft_strchr(s[fd], '\n'))
-	{
-		*line = ft_strsub(s[fd], 0, ft_strchr(s[fd], '\n') - s[fd]);
-		s[fd] = ft_strsub(s[fd], ft_strchr(s[fd], '\n') - s[fd] + 1,
-				ft_strlen(s[fd]));
-	}
-	else
-		*line = ft_strdup(s[fd]);
 	if (ret == -1)
 		return (-1);
-	return (ret != 0 ? 1 : 0);
+	*line = (ft_strchr((del = s[fd]), '\n') ? ft_strsub(s[fd], 0,
+			ft_strchr(s[fd], '\n') - s[fd]) : ft_strdup(s[fd]));
+	if (ft_strchr(s[fd], '\n'))
+		s[fd] = ft_strsub(s[fd], ft_strchr(s[fd], '\n') - s[fd] + 1,
+				ft_strlen(s[fd]));
+	else
+		s[fd] = ft_strsub(s[fd], ft_strlen(*line), ft_strlen(s[fd]));
+	free (del);
+	return (!ret && !ft_strlen(*line) && !ft_strlen(s[fd]) ? 0 : 1);
 }
