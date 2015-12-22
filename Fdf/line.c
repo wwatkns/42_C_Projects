f3e9afee397a134a5cef49f25a6e8b3b933c0b14
wwatkins42 <wwatkins@student.42.fr>
2015-12-22 09:03:50 +0100

    Update line.c

--- Fdf/line.c
+++ Fdf/line.c
@@ -23,11 +23,11 @@
 	line.dy = -ABS((line.p1.y - line.p0.y));
 	sx = (line.p0.x < line.p1.x ? 1 : -1);
 	sy = (line.p0.y < line.p1.y ? 1 : -1);
-	err = (line.dx > line.dy ? line.dx : -line.dy) / 2;
+	err = line.dx + line.dy;
 	while (line.p0.x != line.p1.x || line.p0.y != line.p1.y)
 	{
 		mlx_pixel_put(e.mlx, e.win, line.p0.x, line.p0.y, color);
-		e2 = err;
+		e2 = err << 1;
 		if (e2 >= line.dy)
 		{
 			err += line.dy;
@@ -39,7 +39,7 @@
 			line.p0.y += sy;
 		}
 	}
-}*/ // Bresenham algorithm more opti ?
+}*/ // Bresenham algorithm more opti (err << 1) = (err * 2) mais plus opti (on shift les bits de 1 a gauche)
 
 void	ft_drawline(t_env e, t_line line, int color)
 {
