478492b5a41d682c5a7973722e004cb61f9929f6
William WATKINS <wwatkins@e1r7p2.42.fr>
2016-01-29 09:45:44 +0100

    ft_atoi.c reduced by 50%, (and optimised speed)

--- RTv1/lib/libft/src/ft_atoi.c
+++ RTv1/lib/libft/src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 12:43:57 by wwatkins          #+#    #+#             */
-/*   Updated: 2016/01/03 13:22:43 by wwatkins         ###   ########.fr       */
+/*   Updated: 2016/01/29 09:42:50 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,16 @@
 
 int	ft_atoi(const char *str)
 {
-	int i;
-	int j;
 	int sign;
 	int value;
 
-	j = 0;
 	sign = 0;
 	value = 0;
-	while (ft_isblank(str[j]))
-		j++;
-	i = j;
-	while (str[i] != '\0')
-	{
-		if ('0' <= str[i] && str[i] <= '9')
-			value = (value * 10) + str[i] - '0';
-		else if (str[i] == '+' && i == j)
-			sign = 0;
-		else if (str[i] == '-' && i == j)
-			sign = 1;
-		else
-			break ;
-		i++;
-	}
+	while (ft_isblank(*str))
+		str++;
+	*str == '-' ? sign = 1 : 0;
+	*str == '-' || *str == '+' ? str++ : 0;
+	while ('0' <= *str && *str <= '9')
+		value = (value * 10) + (*(str++) - '0');
 	return (sign == 0 ? value : -value);
 }
