8d6a67298422c5c15c49d3bed07e1239c283c085
William WATKINS <wwatkins@e1r7p2.42.fr>
2016-01-29 16:02:55 +0100

    Libft optimisation

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,22 +6,19 @@
 /*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 14:15:55 by wwatkins          #+#    #+#             */
-/*   Updated: 2016/01/29 13:21:15 by wwatkins         ###   ########.fr       */
+/*   Updated: 2016/01/29 15:56:12 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if ((unsigned char)c == s[i])
-			return ((char*)&s[i]);
-		i++;
+		if ((unsigned char)c == *s)
+			return ((char*)s);
+		s++;
 	}
 	if ((unsigned char)c == 0)
-		return ((char*)&s[i]);
+		return ((char*)s);
 	return (0);
 }
