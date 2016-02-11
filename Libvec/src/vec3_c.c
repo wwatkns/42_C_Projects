fed0f36a542efa34c02675ba480a17277cb9c7d6
William WATKINS <wwatkins@e1r7p4.42.fr>
2016-02-11 14:14:25 +0100

    Libvec updated

---
+++ Libvec/src/vec3_c.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec3_c.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wwatkins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/09 18:31:06 by wwatkins          #+#    #+#             */
+/*   Updated: 2016/02/10 16:22:00 by wwatkins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libvec.h"
+
+void	vec3_clamp(t_vec3 *vec3, double min, double max)
+{
+	vec3->x > max ? vec3->x = max : 0;
+	vec3->y > max ? vec3->y = max : 0;
+	vec3->z > max ? vec3->z = max : 0;
+	vec3->x < min ? vec3->x = min : 0;
+	vec3->y < min ? vec3->y = min : 0;
+	vec3->z < min ? vec3->z = min : 0;
+}
+
+t_vec3	vec3_cross(t_vec3 vec3a, t_vec3 vec3b)
+{
+	t_vec3	vec3;
+
+	vec3.x = vec3a.y * vec3b.z - vec3a.z * vec3b.y;
+	vec3.y = vec3a.z * vec3b.x - vec3a.x * vec3b.z;
+	vec3.z = vec3a.x * vec3b.y - vec3a.y * vec3b.x;
+	return (vec3);
+}
+
+t_vec3	vec3_reflect(t_vec3 d, t_vec3 n)
+{
+	return (vec3_sub(d, vec3_fmul(n, 2.0 * vec3_dot(d, n))));
+}
+
+t_vec3	vec3_norm(t_vec3 vec3)
+{
+	t_vec3	n;
+	double	magnitude;
+
+	magnitude = vec3_magnitude(vec3);
+	n.x = vec3.x / magnitude;
+	n.y = vec3.y / magnitude;
+	n.z = vec3.z / magnitude;
+	return (n);
+}
