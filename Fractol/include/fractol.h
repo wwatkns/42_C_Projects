13c37ed3df86f2b7ed4facee5f6ef81288797378
William WATKINS <wwatkins@e1r7p4.42.fr>
2016-03-02 17:20:16 +0100

    makefile modifications, nolib rule was not recompiling .o

--- Fractol/include/fractol.h
+++ Fractol/include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: wwatkins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/11 09:25:57 by wwatkins          #+#    #+#             */
-/*   Updated: 2016/03/02 14:15:02 by wwatkins         ###   ########.fr       */
+/*   Updated: 2016/03/02 17:16:30 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # include <pthread.h>
 
 # define ABS(x) (x < 0 ? -x : x)
-# define THREADS_NUM 8
+# define THREADS_NUM 16
 
 typedef struct	s_mouse
 {
